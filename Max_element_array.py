af73a0b40603f92b9cb48af80eb71dc9f5520d47
pratikshakhandge <pratikshakhandge2006@gmail.com>
2026-01-30 23:59:41 +0530

    Array Basic Program

---
+++ Max_element_array.py
@@ -0,0 +1,8 @@
+arr = [10, 25, 5, 40, 15]
+max = arr[0]
+
+for i in arr:
+    if i > max:
+        max = i
+
+print("Maximum =", max)
---
+++ count_array_elem.py
@@ -0,0 +1,7 @@
+arr = [5, 10, 15, 20]
+count = 0
+
+for i in arr:
+    count += 1
+
+print("Total elements =", count)
