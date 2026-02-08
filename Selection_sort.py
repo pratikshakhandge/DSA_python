2f4462de476f3d77a8bd1281efc1d24caba2f307
Pratiksha Khandge <pratikshakhandge2006@gmail.com>
2026-02-08 23:55:00 +0530

    Implement selection sort algorithm in Python

---
+++ Selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+arr = [29, 10, 14, 37, 13]
+selection_sort(arr)
+
+print("Sorted array:", arr)
